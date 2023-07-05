dcb4de61af0a6579a7db0751e6a82db8440a3f76
gaurav phuyal <Gaurav Phuyal>
2023-07-05 20:38:55 +0545

    Completed md to html, entry page, index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
+
+markdowner = Markdown()
 
 
 def index(request):
@@ -8,3 +10,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entries(request, name):
+    if name in util.list_entries():
+        return render(request,'encyclopedia/entries.html',{
+            "exist" : True,
+            "name" : name,
+            "markdown" : markdowner.convert(util.get_entry(name))
+        })
+    else:
+        return render(request,'encyclopedia/entries.html',{
+            "name" : name,
+            "exist" : False,
+        })
